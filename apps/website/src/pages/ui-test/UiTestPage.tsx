a7e7c43decfbc84287b0703d4551e783ed019e10
preranaarchitects <preranaarchitects@gmail.com>
2026-06-07 19:44:07 +0530

    Initial Commit

--- apps/website/src/pages/ui-test/UiTestPage.tsx
+++ apps/website/src/pages/ui-test/UiTestPage.tsx
@@ -1,18 +1,16 @@
-import { Navbar, Logo } from "@paa/ui";
+import { Button, Logo, Navbar } from "@paa/ui";
 
 import branding from "../../../../../packages/ui/src/data/branding.json";
 import navbar from "../../data/navbar.json";
 
 export function UiTestPage() {
   return (
-
     <div className="min-h-screen">
-      <Navbar
-        logo={<Logo logo={branding.logo} />}
-        navigation={navbar}
-      />
-    </div>
+      <Navbar logo={<Logo logo={branding.logo} />} navigation={navbar} />
 
-    
+      <main className="p-8">
+        <Button variant="primary">Get a Quote</Button>
+      </main>
+    </div>
   );
 }
